647ab90c0fa6001b203d7febaefd5613a33c0d76
rezaya92 <ryarbakhsh@yahoo.com>
2021-10-14 21:03:12 +0330

    added main guard

--- main.py
+++ main.py
@@ -3,3 +3,7 @@
 def get_inputs():
     operand1, operator, operand2 = input("give me your hardest calculation problem: ").split()
     return operator, float(operand1), float(operand2)
+
+
+if __name__ == '__main__':
+    operator, operand1, operand2 = get_inputs()
