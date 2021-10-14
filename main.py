efa4326fbafe2c5c79c71511102b2a8fff644b57
rezaya92 <ryarbakhsh@yahoo.com>
2021-10-14 21:01:41 +0330

    added get_inputs function

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # SELab experiment 1
+
+def get_inputs():
+    operand1, operator, operand2 = input("give me your hardest calculation problem: ").split()
+    return operator, float(operand1), float(operand2)
